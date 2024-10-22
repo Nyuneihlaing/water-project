694de32c619b21cd192463bd9c32f061cd9c6a43
Steven Pham <steven.h.pham@sjsu.edu>
2024-10-21 19:58:34 -0700

    Added Menubar + other pages, which are empty

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CalculatePage from './pages/calculatePage';
+import HistoryPage from './pages/historyPage';
+import OptionsPage from './pages/optionsPage';
+import MenuBar from './components/Menubar';
 
 function App() {
   return (
     <Router>
       <div>
+        <MenuBar />
+
         <Routes>
-        <Route path="/" element={<CalculatePage />} />
+          <Route path="/" element={<CalculatePage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/options" element={<OptionsPage />} />
         </Routes>
       </div>
     </Router>
---
+++ client/src/pages/historyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HistoryPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-3xl font-bold">History Page</h1>
+    </div>
+  );
+}
+
+export default HistoryPage;
